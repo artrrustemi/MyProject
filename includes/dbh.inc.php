d97905b113dd0b766d2a4e9215800b3777605c74
artrrustemi <85810156+artrrustemi@users.noreply.github.com>
2021-10-11 14:00:08 +0200

    Create dbh.inc.php

---
+++ includes/dbh.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "phpproject01";
+
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+if (!$conn){
+	die("Conncetion faild: " .mysqli_connect_error());
+}
